b053b8409ed72ac767fc9b6e4664d47e89506cda
YashNagar123 <nagaryash929@gmail.com>
2025-12-19 00:22:06 +0530

    Time: 6 ms (27.96%) | Memory: 47 MB (11.51%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int c =0;
+        for(int i =0;i<seats.length;i++){
+            c += Math.abs(seats[i] - students[i]); // returns mod of value
+        }
+
+        return c;
+
+    }
+}
