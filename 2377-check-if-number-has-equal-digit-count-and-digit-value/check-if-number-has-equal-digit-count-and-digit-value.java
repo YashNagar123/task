00478a484638b880553ad1c14f4a3ce00626e563
YashNagar123 <nagaryash929@gmail.com>
2025-12-19 22:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (6.48%) - LeetSync

---
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean digitCount(String num) {
+
+        int [] f = new int [26];
+
+        for(char ch : num.toCharArray()){
+         f[ch-'0']++;
+        } 
+
+        
+        for(int i =0;i<num.length();i++){
+            if( num.charAt(i) - '0' != f[i])  return false;
+        }
+
+        return true;
+    }
+}
