02e05d173ddebbe841401432f64182e836dc2a82
YashNagar123 <nagaryash929@gmail.com>
2025-12-26 22:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (9.88%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
@@ -0,0 +1,31 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+
+        int s = 1;
+        int l = n;
+        int ans = 0;
+
+        while(s <= l){
+
+            int m = s+(l-s)/2;
+
+            if(guess(m) == 0) {
+                ans = m;
+                break;
+            }else if(guess(m) == -1) l = m - 1;
+            else s = m + 1;
+        }
+
+        return ans;
+        
+    }
+}
